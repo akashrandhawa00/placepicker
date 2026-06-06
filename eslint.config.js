b54547f22da1b95a0e1bd0385d523f23d4ab4b9c
Akash Randhawa <93700117+akashrandhawa00@users.noreply.github.com>
2026-06-06 18:55:13 -0400

    connecting to backend

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  {
+    ignores: ["dist"],
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,jsx}"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 
 export default function ProgressBar({ timer }) {
   const [remainingTime, setRemainingTime] = useState(timer);
+
   useEffect(() => {
     const interval = setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 10);
